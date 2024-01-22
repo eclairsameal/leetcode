40b915110846d10ebc458d586b65901836636f64
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2024-01-22 10:26:58 +0900

    Time: 68 ms (81.61%) | Memory: 17.4 MB (48.29%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -5,6 +5,8 @@ class Solution:
         while forward < after :
             if nums[forward] % 2 == 1 and nums[after] % 2 == 0:
                 nums[forward], nums[after] = nums[after], nums[forward]
+                forward += 1
+                after -= 1
             if nums[forward] % 2 == 0:
                 forward += 1
             if nums[after] % 2 == 1:
